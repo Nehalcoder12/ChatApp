62b054c73ffa65d904303311babb26e9206863ce
Nehalcoder12 <92616003+Nehalcoder12@users.noreply.github.com>
2024-12-19 06:22:50 +0530

    Update connectToMongoDB.js

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb://127.0.0.1:27017/");
+		await mongoose.connect("mongodb+srv://1si21ec065:bjThXb20nsgudqvj@cluster0.krgwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
